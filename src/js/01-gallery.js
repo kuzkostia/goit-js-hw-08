d65201f7c95543559bba3e225a0848e091433c2a
kuzkostia <112246522+kuzkostia@users.noreply.github.com>
2023-03-15 10:18:41 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML(
+  'beforeend',
+  createImageGallery(galleryItems)
+);
+
+function createImageGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}" style= "box-shadow: none;" style="border-radius: 4px;">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>
+</div>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
