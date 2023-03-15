65850e3e58ddee4602397fabe4c77c0abb1a4fa1
kuzkostia <112246522+kuzkostia@users.noreply.github.com>
2023-03-15 10:43:58 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,14 @@
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
-const VCT_KEY = 'videoplayer-current-time';
+const KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 const onPlay = function (event) {
-  localStorage.setItem(VCT_KEY, event.seconds);
+  localStorage.setItem(KEY, event.seconds);
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(JSON.parse(localStorage.getItem(VCT_KEY)) || 0);
+player.setCurrentTime(JSON.parse(localStorage.getItem(KEY)) || 0);
